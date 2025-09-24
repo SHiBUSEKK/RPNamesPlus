b936829b4c7a2e920102c24c082bf8548b922e15
SHiBUSEKK <max@stolarski.info>
2025-09-24 14:58:08 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# RPNames+
+
+## What's RPNames+
+
+**RPNames+** is a plugin for [EXILED](https://github.com/ExMod-Team/EXILED) that adds the roleplay nicknames randomizer.
+
---
+++ RPNames+/RPNames+.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Exiled.API.Features;
+using Exiled.Events.EventArgs.Player;
+using PlayerRoles;
+using UnityEngine;
+
+namespace RPNamesPlus
+{
+    public class Plugin : Plugin<Config>
+    {
+        public override string Author => "Shibusekk";
+        public override string Name => "RPNames+";
+        public override string Prefix => "RPNames+";
+        public override Version Version => new Version(1, 0, 0);
+        public override Version RequiredExiledVersion => new Version(9, 9, 2);
+
+
+        public override void OnEnabled()
+        {
+            Exiled.Events.Handlers.Player.ChangingRole += OnChangingRole;
+            Log.Info("RPNames+ has been enabled. Thank you for using my plugin.");
+        }
+
+        public override void OnDisabled()
+        {
+            Exiled.Events.Handlers.Player.ChangingRole -= OnChangingRole;
+        }
+
+        private void OnChangingRole(ChangingRoleEventArgs ev)
+        {
+            if (ev.NewRole == RoleTypeId.Spectator || ev.NewRole == RoleTypeId.Overwatch)
+            {
+                ev.Player.DisplayNickname = ev.Player.Nickname;
+                return;
+            }
+
+            ApplyRoleplayName(ev.NewRole, ev.Player);
+        }
+
+        private void ApplyRoleplayName(RoleTypeId role, Player player)
+        {
+            string roleName = role.ToString();
+
+            if (role == RoleTypeId.ClassD)
+            {
+                string dNumber = UnityEngine.Random.Range(1000, 10000).ToString();
+                player.DisplayNickname = "D-" + dNumber;
+                return;
+            }
+
+            if (!Config.RPNamesPlus.ContainsKey(roleName))
+                return;
+
+            var roleConfig = Config.RPNamesPlus[roleName];
+            if (roleConfig.Names == null || roleConfig.Names.Count == 0)
+                return;
+
+            string randomName = roleConfig.Names[UnityEngine.Random.Range(0, roleConfig.Names.Count)];
+            player.DisplayNickname = roleConfig.Prefix + randomName;
+        }
+    }
+}
